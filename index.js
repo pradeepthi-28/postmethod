3411186de5f6034fc264bb88c0874259ba0a4d87
Abhinaireddy-Duba <abhinaireddy2002@gmail.com>
2022-11-25 16:53:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const bodyparser=require("body-parser")
+const port=3000;
+
+app.use(bodyparser.urlencoded({
+	extended:true
+}))
+app.use(bodyparser.json())
+
+app.use(cors())
+
+app.get("/",(req,res)=>{
+	res.send("server is running")
+})
+
+app.post("/newData",(req,res)=>{
+	//const name=req.body.name,age=req.body.age
+	const {name,password,cors}=req.body;
+	console.log(name,password,cors)
+	res.send("added Successfully")
+})
+app.listen(port,()=>console.log("server is loading onport 3000"))
+
